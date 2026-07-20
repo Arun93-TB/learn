0f4db717426b0769ef077e10d8022c531753ed24
Arun93-TB <legendarunkumar47@gmail.com>
2026-07-20 11:34:31 +0530

    alter

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async function(event){
+
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("/login",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            username,
+            password
+        })
+
+    });
+
+    const result = await response.json();
+
+    const message = document.getElementById("message");
+
+    if(result.success){
+
+        message.style.color="lime";
+        message.textContent=result.message;
+
+    }else{
+
+        message.style.color="red";
+        message.textContent=result.message;
+
+    }
+
+});
